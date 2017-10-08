f692c542f90c8f7c66cad7aa5570eb62a25ecbac
NITRO15\flanker <rgunczer@gmail.com>
2017-10-08 20:54:00 +0200

    refactored level loading

--- core/src/com/mygdx/game/LevelLoader.java
+++ core/src/com/mygdx/game/LevelLoader.java
@@ -31,40 +31,43 @@ public class LevelLoader {
     public Array<WayPointInfo> wayPoints;
     public Array<PlantInfo> plants;
 
+
+    private void loadWaypoints(JsonValue root) {
+        JsonValue entry = root.get("WayPoints");
+        wayPoints = new Array<WayPointInfo>(entry.size);
+        for(JsonValue wp = entry.child; wp != null; wp = wp.next) {
+            int id = wp.getInt("id");
+            JsonValue array = wp.get("pos");
+            float x = array.getInt(0);
+            float y = array.getInt(1);
+            wayPoints.add(new WayPointInfo(id, x, y));
+        }
+    }
+
+    private void loadPlants(JsonValue root) {
+        JsonValue entry = root.get("Plants");
+        plants = new Array<PlantInfo>(entry.size);
+        for(JsonValue plant = entry.child; plant != null; plant = plant.next) {
+            String type = plant.getString("type");
+            JsonValue array = plant.get("pos");
+            float x = array.getInt(0);
+            float y = array.getInt(1);
+            plants.add(new PlantInfo(type, x, y));
+        }
+    }
+
     public void load(String fileName) {
         FileHandle file = Gdx.files.internal(fileName);
 
         JsonReader jsonReader = new JsonReader();
-        JsonValue map = jsonReader.parse(file);
+        JsonValue root = jsonReader.parse(file);
 
-        for(JsonValue entry = map.child; entry != null; entry = entry.next) {
-            System.out.println(entry.name);
-            if (entry.name.equals("WayPoints")) {
-                wayPoints = new Array<WayPointInfo>(entry.size);
-                for(JsonValue wp = entry.child; wp != null; wp = wp.next) {
-                    int id = wp.getInt("id");
-                    JsonValue array = wp.get("pos");
-                    float x = array.getInt(0);
-                    float y = array.getInt(1);
-                    wayPoints.add(new WayPointInfo(id, x, y));
-                }
-            }
-            if (entry.name.equals("Plants")) {
-                plants = new Array<PlantInfo>(entry.size);
-                for(JsonValue plant = entry.child; plant != null; plant = plant.next) {
-                    String type = plant.getString("type");
-                    JsonValue array = plant.get("pos");
-                    float x = array.getInt(0);
-                    float y = array.getInt(1);
-                    plants.add(new PlantInfo(type, x, y));
-                }
-            }
-        }
+        loadWaypoints(root);
+        loadPlants(root);
 
         System.out.println("level stat:");
         System.out.println("waypoints: " + wayPoints.size);
         System.out.println("plants: " + plants.size);
         System.out.println("-----------");
     }
-
 }
